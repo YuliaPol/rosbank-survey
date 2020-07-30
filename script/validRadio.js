02eccb1c92e44dd81591615b7e2afdd6944a42d3
YuliaPol <julia.polyeshko@gmail.com>
2020-07-30 15:17:05 +0300

    Checkboxs

--- script/validRadio.js
+++ script/validRadio.js
@@ -3,6 +3,7 @@ form.addEventListener('submit', function (e) {
 
     e.preventDefault();
     var el = document.querySelectorAll('[data-reqired]');
+    var elCheck = document.querySelectorAll('.second-question input[type="checkbox"]');
     var erroreArrayElemnts = [];
 
     for (var i = 0; i < el.length; i++) {
@@ -18,9 +19,7 @@ form.addEventListener('submit', function (e) {
                 if($(el[i]).parents('.range-wrp').length > 0){
                     digs = $(el[i]).parents('.range-wrp').find('.error-text');
                 }
-                else if($(el[i]).parents('.input-radio-group').length > 0 && $('.first-question input').is(':checked')) {
-                    digs = $(el[i]).parents('.input-radio-group').find('.error-text');
-                }
+                
                 for (var d = 0; d < digs.length; d++) {
                     $(digs[d]).fadeIn(300);
                 }
@@ -31,7 +30,37 @@ form.addEventListener('submit', function (e) {
                             if($(e.target).parents('.range-wrp').length > 0){
                                 $(e.target).parents('.range-wrp').find('.error-text').fadeOut(300);
                             }
-                            else if($(e.target).parents('.input-radio-group').length > 0 && $('.first-question input').is(':checked')) {
+                        });
+                    }
+                }
+            }
+        } 
+    }
+
+    for (var i = 0; i < elCheck.length; i++) {
+        if (elCheck[i].tagName === 'INPUT') {
+
+            var name = elCheck[i].getAttribute('name');
+
+            if (document.querySelectorAll('[name=' + name + ']:checked').length === 0 ) {
+
+
+                var digs;
+
+                if($('.first-question input').is(':checked') && $('.second-question input[type="checkbox"]:checked').length == 0){
+                    digs = $(elCheck[i]).parents('.input-radio-group').find('.error-text');
+                    erroreArrayElemnts.push(elCheck[i]);
+                }
+                if(digs){
+                    for (var d = 0; d < digs.length; d++) {
+                        $(digs[d]).fadeIn(300);
+                    }
+                }
+
+                for (var e = 0; e < 10; e++) {
+                    if(document.querySelectorAll('[name=' + name + ']')[e]){
+                        document.querySelectorAll('[name=' + name + ']')[e].addEventListener('change', (e) => {
+                            if($(e.target).parents('.input-radio-group').length > 0 && $('.first-question input').is(':checked')) {
                                 $(e.target).parents('.input-radio-group').find('.error-text').fadeOut(300);
                             }
                         });
@@ -40,8 +69,6 @@ form.addEventListener('submit', function (e) {
             }
         } 
     }
-
-
     if (erroreArrayElemnts.length > 0) {
         window.scrollTo(0, erroreArrayElemnts[0].parentElement.parentElement.getBoundingClientRect().top);
     }
@@ -52,5 +79,10 @@ form.addEventListener('submit', function (e) {
 $('.first-question input').change(function(e){
     if($(this).is(':checked')) {
         $('.second-question').fadeIn(300);
+    }
+});
+$('.check-answer').change(function(e){
+    if($(this).is(':checked')) {
+        $('.detail-answer').fadeIn(300);
     }
 });
